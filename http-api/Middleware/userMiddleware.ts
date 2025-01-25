f069fd4014ec13770c68e6a1c0c5f323318ac69c
Ashan Griffith Mundle <ashgriff2154@gmail.com>
2025-01-25 15:31:29 +0000

    add joi validation schema + add user middleware

---
+++ http-api/Middleware/userMiddleware.ts
@@ -0,0 +1,24 @@
+import joi, { ObjectSchema } from "joi"
+import { Request, Response, NextFunction }from "express"
+
+
+
+
+
+
+
+const validateSchema = (schema: ObjectSchema) =>{
+    return (req: Request, res: Response, next: NextFunction) =>{
+        const {error} = schema.validate(req.body, {abortEarly: false})
+        if(error){
+            res.status(400).json({errors: error.details.map((details) => ({
+                message: details.message,
+                field: details.path.join(".")
+            })
+        )})
+        return
+        }
+       
+        next()
+    }
+}
---
+++ http-api/Schemas/userSchema.ts
@@ -0,0 +1,11 @@
+
+import joi from "joi"
+
+export const createUser = joi.object({
+
+    id: joi.string().required(),
+    email: joi.string().min(8).max(255).required(),
+    username: joi.string().min(8).max(255).required(),
+    password: joi.string().min(8).max(255).required(),
+    mobile: joi.number().min(8).max(255).required()
+})
